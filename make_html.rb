2d5ea0eb4c1d3cf2e9dee0de5d3e28e9bb25ecf1
akkagi0416 <akkagi0416@gmail.com>
2019-10-04 19:01:39 +0900

    pick up random icon

--- make_html.rb
+++ make_html.rb
@@ -1,6 +1,18 @@
 require 'erb'
 require 'json'
 
+MEMBER_MAX = 20
+
+#
+# make list json
+#
+# lists
+#   [
+#     ['img/a1.png', 'name1'],
+#     ['img/a2.png', 'name2'],
+#     ...
+#   ]
+#
 lists = []
 open('list.csv').each do |line|
   img  = line.chomp.split(',')[0]
@@ -11,6 +23,9 @@ end
 
 json_lists = lists.to_json
 
+#
+# output html
+#
 erb = ERB.new(DATA.read)
 open('index.html', 'w'){|f| f.write erb.result(binding) }
 
@@ -53,12 +68,15 @@ __END__
     .icon{
       position: absolute;
     }
-    <% 0.upto(19) do |i| %>
+    <% 0.upto(MEMBER_MAX - 1) do |i| %>
       .icon<%= i %>{
-        top: 110px;
-        right: <%= 105 + (i % 5)*111 %>px;
+        top:  <%= 110 + (i / 5) * 115 %>px;
+        left: <%= 303 + (i % 5) * 111 %>px;
       }
     <% end %>
+    .invisible{
+      display: none;
+    }
   </style>
 </head>
 <body>
@@ -67,12 +85,15 @@ __END__
   <main>
     <div class="screen">
       <div class="bg"></div>
-      <% lists[0..19].each_with_index do |list, i| %>
-        <div class="icon icon<%= i %>"><img src="<%= list[0] %>" alt="<%= list[1] %>"></div>
+      <% lists[0..(MEMBER_MAX - 1)].each_with_index do |list, i| %>
+        <div class="icon icon<%= i %> invisible"><img id="img_icon<%= i %>" src="<%= list[0] %>" alt="<%= list[1] %>"></div>
       <% end %>
     </div>
     <img id="img_aiu" src="" alt="">
-    <button id="button">button</button>
+    <div id="menu">
+      <button id="button">button</button>
+      <input type="number" id="member_cnt" name="member_cnt" value="3" min="1" max="20">
+    </div>
   </main>
   <footer>
   </footer>
@@ -81,10 +102,37 @@ __END__
   </script>
   <script>
     document.getElementById('button').addEventListener('click', function(){
+      /* for check part
       let img = document.getElementById('img_aiu')
       let idx = getRandomIndex()
       img.src = json_lists[idx][0]
+      img.alt = json_lists[idx][1]
+      */
+
+      // reset .invisible
+      for(let i = 0; i < <%= MEMBER_MAX %>; i++){
+        let icon = document.getElementsByClassName('icon')[i]
+        if(!icon.classList.contains('invisible')){
+          icon.classList.add('invisible')
+        }
+      }
+
+      // pickup random icons
+      let cnt = document.getElementById('member_cnt').value
+      for(let i = 0; i < cnt; i++){
+        let icon = document.getElementsByClassName('icon')[i]
+        // invisible -> visible
+        icon.classList.remove('invisible')
+
+        let img = document.getElementById('img_icon' + i)
+        let idx = getRandomIndex()
+        console.log(img)
+        img.src = json_lists[idx][0]
+        img.alt = json_lists[idx][1]
+      }
     })
+
+    // get random icons index
     function getRandomIndex(){
       return Math.floor(Math.random() * json_lists.length)
     }
